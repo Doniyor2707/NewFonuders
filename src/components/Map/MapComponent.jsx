0311f34f9772fd8ae8e4e0981b53e175044a3fa8
Doniyor <dbek59141@gmail.com>
2025-03-11 10:35:45 -0700

    Founders icon maps

--- src/components/Map/MapComponent.jsx
+++ src/components/Map/MapComponent.jsx
@@ -1,5 +1,16 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Tooltip } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+// Qizil marker icon
+const redIcon = new L.Icon({
+  iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
+  shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41],
+});
 
 const MapComponent = () => {
   const position = [41.306317, 69.236425];
@@ -12,8 +23,10 @@ const MapComponent = () => {
       zoomControl={false}
     >
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      <Marker position={position}>
-        <Popup>Founders Language School</Popup>
+      <Marker position={position} icon={redIcon}>
+        <Tooltip permanent >
+          Founders Language School
+        </Tooltip>
       </Marker>
     </MapContainer>
   );
